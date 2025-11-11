d67c3e9deb956af08c49f06c93ff5a563f2e5fe6
tominehaug <tomine_haug@live.no>
2025-11-11 12:03:10 +0100

    updates

--- js/shopping-cart.js
+++ js/shopping-cart.js
@@ -1,25 +1,42 @@
 const cart = JSON.parse(localStorage.getItem("cart")) || [];
 
+console.log(cart);
+
 const itemList = document.getElementById("item-list");
 itemList.innerHTML="";
 
+function updateSubtotal(){
+    let subtotal = 0;
+    cart.forEach(item => {
+        subtotal += item.price * item.quantity;
+    })
+
+    const subtotalDisplay = document.getElementById("subtotal");
+    subtotalDisplay.textContent = "SUBTOTAL: " + subtotal + "kr";
+}
+
+updateSubtotal();
+
 function displayItems(items) {
     itemList.innerHTML = "";
-    const itemContainer = document.createElement("div");
-    itemList.appendChild(itemContainer);
     
-    cart.forEach((item) => {
+    items.forEach((item) => {
+        const itemContainer = document.createElement("div");
+        itemContainer.classList.add("item-container");
+        itemList.appendChild(itemContainer);
+
         const image = document.createElement("img");
         image.src = item.imageUrl;
         image.alt = item.imageAlt;
         itemContainer.appendChild(image);
 
+        const infoDiv = document.createElement("div");
+        infoDiv.classList.add("info");
+        itemContainer.appendChild(infoDiv);
+    
         const name = document.createElement("h2");
         name.textContent = item.name;
-        itemContainer.appendChild(name);
-
-        const infoDiv = document.createElement("div");
-        itemContainer.appendChild(infoDiv);
+        infoDiv.appendChild(name);
         
         const size =document.createElement("p");
         size.textContent = "Size: " + item.size;
@@ -32,12 +49,18 @@ function displayItems(items) {
         const price = document.createElement("p");
         price.textContent = "Price: " + item.price + "kr";
         infoDiv.appendChild(price);
-    });
-
+        
+        const deleteButton = document.createElement("button");
+        deleteButton.id = "delete-button";
+        deleteButton.textContent = "Delete";
+        infoDiv.appendChild(deleteButton);
+    })
+        
 }
 
+    
 if (cart.length === 0) {
     itemList.innerHTML = "<p>Your cart is empty.</p>";
 }else{
-    displayItems(items);
+    displayItems(cart);
 }
