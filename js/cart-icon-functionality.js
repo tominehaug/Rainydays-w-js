46561c667e293a75bee95ccaeebd115dfad691c6
tominehaug <tomine_haug@live.no>
2026-03-01 12:35:11 +0100

    fixed HTML structure by moving footer, moved inline styles to css, gave sign-in page unique title, updated class for cart-icon

--- js/cart-icon-functionality.js
+++ js/cart-icon-functionality.js
@@ -2,22 +2,19 @@ let cartContent = JSON.parse(localStorage.getItem("cart")) || [];
 
 const url = window.location.href;
 
-let cartIcon = document.querySelector(".cartIcon");
-cartIcon.innerHTML = '';
+let cartIcon = document.querySelector(".cart-icon");
+cartIcon.innerHTML = "";
 
 const iconImg = document.createElement("img");
 iconImg.classList.add("cart");
 cartIcon.appendChild(iconImg);
 
-if ((cartContent.length === 0) && !url.includes('checkout')){
-    iconImg.src = "/assets/shopping-cart-icon.png";
-}
-else if ((cartContent.length > 0) && !url.includes('checkout')){
-    iconImg.src = "/assets/shopping-cart-icon5.png"
-}
-else if ((cartContent.length === 0) && url.includes('checkout')){
-    iconImg.src = "../../assets/shopping-cart-icon-4.png";
-}
-else if ((cartContent.length > 0) && url.includes('checkout')){
-    iconImg.src = "../../assets/shopping-cart-icon6.png";
+if (cartContent.length === 0 && !url.includes("checkout")) {
+  iconImg.src = "/assets/shopping-cart-icon.png";
+} else if (cartContent.length > 0 && !url.includes("checkout")) {
+  iconImg.src = "/assets/shopping-cart-icon5.png";
+} else if (cartContent.length === 0 && url.includes("checkout")) {
+  iconImg.src = "../../assets/shopping-cart-icon-4.png";
+} else if (cartContent.length > 0 && url.includes("checkout")) {
+  iconImg.src = "../../assets/shopping-cart-icon6.png";
 }
