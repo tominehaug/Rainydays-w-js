ddfbf1cabb9e6213bafa804ea73f0ab5042d531f
tominehaug <tomine_haug@live.no>
2026-03-01 12:45:38 +0100

    attempt at cart-icon path

--- js/cart-icon-functionality.js
+++ js/cart-icon-functionality.js
@@ -9,10 +9,10 @@ const iconImg = document.createElement("img");
 iconImg.classList.add("cart");
 cartIcon.appendChild(iconImg);
 
-const icon = "/assets/shopping-cart-icon.png";
-const icon4 = "/assets/shopping-cart-icon-4.png";
-const icon5 = "/assets/shopping-cart-icon5.png";
-const icon6 = "/assets/shopping-cart-icon6.png";
+const icon = "./assets/shopping-cart-icon.png";
+const icon4 = "./assets/shopping-cart-icon-4.png";
+const icon5 = "./assets/shopping-cart-icon5.png";
+const icon6 = "./assets/shopping-cart-icon6.png";
 
 if (cartContent.length === 0 && !url.includes("checkout")) {
   iconImg.src = icon;
